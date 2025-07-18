6173cf5a908e6c9c83a93f994852ed25e7b581cf
limzishen <limzishen27@gmail.com>
2025-07-18 23:26:56 +0800

    trying something

--- app.py
+++ app.py
@@ -77,5 +77,5 @@ def predict():
         return jsonify({"error": str(e)}), 500
 
 
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+# if __name__ == '__main__':
+#     app.run(port=5000, debug=True)
